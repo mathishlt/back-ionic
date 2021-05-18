d4c6b203f258218a29e17672ab69381418d65e60
Antoine <coderademii@gmail.com>
2021-05-18 21:15:06 +0200

    add fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,38 +5,44 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+  private $passwordEncoder;
+  public function __construct(UserPasswordEncoderInterface $passwordEncoder) {
+    $this->passwordEncoder = $passwordEncoder;
+  }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
-                            $user->setNom("Ratieuville");
-                            $user->setPrenom("Antoine");
-                            $user->setMail("antoine.ratieuville76@gmail.com");
-                            $user->setPassword("azerty");
-                            $user->setBirthdate(new \DateTime('2000-08-17'));
-                            $user->setTaille(175);
-                            $user->setPoids(70);
-                            $user->setCigarette(true);
-                            $user->setAlcool(true);
-                            $user->setAdmin(true);
-                            $manager->persist($user);
+        $user->setEmail("antoine.ratieuville76@gmail.com");
+        $user->setRoles([]);
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'azerty'));
+        $user->setNom("Ratieuville");
+        $user->setPrenom("Antoine");
+        $user->setBirthdate(new \DateTime('2000-08-17'));
+        $user->setTaille(175);
+        $user->setPoids(70);
+        $user->setCigarette(true);
+        $user->setAlcool(true);
+        $manager->persist($user);
 
         for ($i = 1; $i < 40; $i++) {
-                    $user = new User();
-                    $user->setNom("Nom".$i);
-                    $user->setPrenom("Prenom".$i);
-                    $user->setMail("nom.prenom".$i."@gmail.com");
-                    $user->setPassword("passwordsécurisédeouf".$i);
-                    $user->setBirthdate(new \DateTime('2000-05-12'));
-                    $user->setTaille(rand(150, 230));
-                    $user->setPoids(rand(45,150));
-                    $user->setCigarette(true);
-                    $user->setAlcool(true);
-                    $user->setAdmin(false);
-                    $manager->persist($user);
-                }
+            $user = new User();
+            $user->setEmail("nom.prenom".$i."@gmail.com");
+            $user->setRoles([]);
+            $user->setPassword($this->passwordEncoder->encodePassword($user, 'azerty'));
+            $user->setNom("Nom".$i);
+            $user->setPrenom("Prenom".$i);
+            $user->setBirthdate(new \DateTime('2000-05-12'));
+            $user->setTaille(rand(150, 230));
+            $user->setPoids(rand(45,150));
+            $user->setCigarette(true);
+            $user->setAlcool(true);
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
